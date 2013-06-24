6c9b2a71ee1c54422c2b0053b6bac1fcf0547236
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-24 16:08:29 -0700

    Added the socket code.

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.get("/", function (request, response) {
     response.render("page");
-}).listen(8080);
+});
+app.use(express.static(__dirname + '/public'));
+
+var io = require('socket.io').listen(app.listen(8080));
+
+io.sockets.on('connection', function(socket){
+	socket.emit('message', { message: 'welcome to the chat' });
+	socket.on('send', function(data) {
+		io.sockets.emit('message', data);
+	});
+});
 
 console.log('Server started');
