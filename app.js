2fbf9e6321965a3c7281d19e959ee8d9b83388c3
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-24 15:47:03 -0700

    Re-factored some code.

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
-var port = 8080;
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.get("/", function (request, response) {
     response.render("page");
-});
-
-app.listen(port);
+}).listen(8080);
 
 console.log('Server started');
